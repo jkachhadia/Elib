f41ca6e941944a5bf03daca72e4b86200a028e87
Jay Kachhadia <jkachhadia>
2015-08-06 21:31:54 +0530

    changes to images

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 
 var app = express();
-var data= require("./data.json");
-app.locals.web=data;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,7 +53,6 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 
 module.exports = app;
